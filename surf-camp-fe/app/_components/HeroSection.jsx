e6accfca16c957f00f8cf17c6918df3389e7dc6c
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-17 23:20:46 +0300

    hero jsx

---
+++ surf-camp-fe/app/_components/HeroSection.jsx
@@ -0,0 +1,15 @@
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <div className="hero__background">
+        <img src="/assets/hero-home.png" alt="" />
+      </div>
+      <div className="hero-headline">
+        <h1>barrel.</h1>
+        <h1>your.</h1>
+        <h1>happiness.</h1>
+      </div>
+    </section>
+  );
+};
+export default HeroSection;
---
+++ surf-camp-fe/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import HeroSection from "./_components/HeroSection";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+    </main>
+  );
+}
