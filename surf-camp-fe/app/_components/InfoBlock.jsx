1436fe6430c36bb95c8303dd8ddd86a4c047a082
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-18 19:00:51 +0300

    info blocks styles

--- surf-camp-fe/app/_components/InfoBlock.jsx
+++ surf-camp-fe/app/_components/InfoBlock.jsx
@@ -1,8 +1,9 @@
 const InfoBlock = ({ data }) => {
   const { headline, text, button, reversed } = data;
+
   return (
     <div className={`info ${reversed ? "info--reversed" : ""}`}>
-      <img className="info__image" src="/info-bllocks/rectangle.png" alt="" />
+      <img className="info__image" src="/info-blocks/rectangle.png" alt="" />
       <div className="info__text">
         <h2 className="info__headline">{headline}</h2>
         {text}
