f549c98272ee0275c877fbed0ec84060bec81403
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-18 15:32:50 +0300

    hero styles

--- surf-camp-fe/app/_components/HeroSection.jsx
+++ surf-camp-fe/app/_components/HeroSection.jsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-const HeroSection = ({ imgSrc, headline, theme = "turquise" }) => {
+const HeroSection = ({ imgSrc, headline, theme = "turquoise" }) => {
   return (
     <section className="hero">
       <div className="hero__background">
         <img src={imgSrc || "/assets/hero-home.png"} alt="" />
       </div>
       <div className={`hero-headline hero__headline--${theme}`}>{headline}</div>
-      <button className={`btn btn--medium btm--${theme}`}>
+      <button className={`btn btn--medium btn--${theme}`}>
         <Link href="/events">BOOK NOW</Link>
       </button>
       <img
