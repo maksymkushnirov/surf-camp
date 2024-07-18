401606e66008fbd1e06fa76308333befd833135e
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-18 06:56:07 +0300

    hero structure jsx

--- surf-camp-fe/app/_components/HeroSection.jsx
+++ surf-camp-fe/app/_components/HeroSection.jsx
@@ -1,14 +1,19 @@
-const HeroSection = () => {
+import Link from "next/link";
+
+const HeroSection = ({ imgSrc, headline, theme = "turquise" }) => {
   return (
     <section className="hero">
       <div className="hero__background">
-        <img src="/assets/hero-home.png" alt="" />
+        <img src={imgSrc || "/assets/hero-home.png"} alt="" />
       </div>
-      <div className="hero-headline">
-        <h1>barrel.</h1>
-        <h1>your.</h1>
-        <h1>happiness.</h1>
-      </div>
+      <div className={`hero-headline hero__headline--${theme}`}>{headline}</div>
+      <button className={`btn btn--medium btm--${theme}`}>
+        <Link href="/events">BOOK NOW</Link>
+      </button>
+      <img
+        className={`hero__logo hero__logo--${theme}`}
+        src="/assets/logo.svg"
+      />
     </section>
   );
 };
--- surf-camp-fe/app/page.jsx
+++ surf-camp-fe/app/page.jsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 import HeroSection from "./_components/HeroSection";
 
 export default function Home() {
+  const heroHeadline = (
+    <>
+      <h1>barrel.</h1>
+      <h1>your.</h1>
+      <h1>happiness.</h1>
+    </>
+  );
   return (
     <main>
-      <HeroSection />
+      <HeroSection headline={heroHeadline} />
     </main>
   );
 }
