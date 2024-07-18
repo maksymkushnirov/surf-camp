6749b73b121bcfc876dfaf44a9fc82d8924a52cf
maksymkushnirov <maksymkushnirov@gmail.com>
2024-07-18 19:26:53 +0300

    30 reusing blocks

--- surf-camp-fe/app/_components/Layout/Header.jsx
+++ surf-camp-fe/app/_components/Layout/Header.jsx
@@ -1,14 +1,20 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const path = usePathname();
   const navItems = [
     { display: "the camp.", slug: "/" },
-    { display: "the experience", slug: "/experience." },
+    { display: "the experience", slug: "/experience" },
     { display: "the blog.", slug: "/blog" },
   ];
 
   return (
-    <header className="header">
+    <header
+      className={`header ${path === "/experience" ? "header--light" : ""}`}
+      // className={`header ${path === "/experience" && "header--light"}`}
+    >
       <img className="header__logo" src="/assets/logo.svg" alt="" />
       <ul className="header__nav">
         {navItems.map((item) => (
