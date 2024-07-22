f0b4d342cd391af7724ed9c1222ba40861ff86df
Maksym Kushnirov <maksymkushnirov@gmail.com>
2024-07-22 15:16:57 +0300

    41 subscription jsx

---
+++ surf-camp-fe/app/_components/Blog/SubscribeToNewsletter.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+const SubscribeToNewsletter = () => {
+  return (
+    <section className="newsletter">
+      <div className="newsletter__info">
+        <h4>subscribe to our newsletter</h4>
+        <p className="copy">
+          Unlock Exclusive Insights and Stay In the Know – Subscribe to Our
+          Newsletter Today to always stay in touch
+        </p>
+      </div>
+      <form className="newsletter__form">
+        <input
+          type="text"
+          className="newsletter__email"
+          placeholder="Enter your E-mail address"
+        />
+      </form>
+      <button className="newsletter__subscribe btn btn--medium btn--turquoise">
+        SUBSCRIBE
+      </button>
+    </section>
+  );
+};
+
+export default SubscribeToNewsletter;
