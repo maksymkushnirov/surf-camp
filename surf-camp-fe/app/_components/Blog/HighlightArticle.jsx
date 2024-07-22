9317f40edd3608df54242a77701ac88d581271df
Maksym Kushnirov <maksymkushnirov@gmail.com>
2024-07-22 12:03:01 +0300

    Update HighlightArticle.jsx

--- surf-camp-fe/app/_components/Blog/HighlightArticle.jsx
+++ surf-camp-fe/app/_components/Blog/HighlightArticle.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
 const HighlightArticle = ({ data }) => {
-    const { headline, excerpt, slug, featuredImage) = data;  return (
+  const { headline, excerpt, slug, featuredImage } = data;
+  return (
     <article className="highlight-article">
       <div className="highlight-article-info">
-        <h3>Headline</h3>
-        <p className="copy">Some Text</p>
-        <Link className="btn btn--medium btn--turquoise" href="/blog/slug">
+        <h3>{headline}</h3>
+        <p className="copy">{excerpt}</p>
+        <Link className="btn btn--medium btn--turquoise" href={`/blog/${slug}`}>
           Read more
         </Link>
       </div>
-      <img src="/assets/hero-experience.png" alt="" />
+      <img src={featuredImage} alt="" />
     </article>
   );
 };
