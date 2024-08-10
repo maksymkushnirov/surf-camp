4768cdce541a81b72840648754b366b986d7f57e
Maksym Kushnirov <maksymkushnirov@gmail.com>
2024-08-10 12:52:24 +0300

    66. Blog Section Homepage JSX

--- fe/app/page.jsx
+++ fe/app/page.jsx
@@ -1,4 +1,4 @@
-// import BlogPreview from "./_components/BlogPreview/BlogPreview";
+import BlogPreview from "./_components/BlogPreview/BlogPreview";
 import HeroSection from "./_components/HeroSection";
 import InfoBlock from "./_components/InfoBlock";
 import { fetchDataFromStrapi, processInfoBlocks } from "@/utils/strapi.utils";
@@ -22,7 +22,7 @@ export default async function Home() {
       {infoBlockData.map((data) => (
         <InfoBlock key={data.id} data={data} />
       ))}
-      {/* <BlogPreview /> */}
+      <BlogPreview />
     </main>
   );
 }
